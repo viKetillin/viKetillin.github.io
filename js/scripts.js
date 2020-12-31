80deb1c22dadf2dc3b226856f4818aa693695c72
Vitória Ketillin Fusco <vitoriaketillin@hotmail.com>
2020-12-31 18:28:21 -0300

    v2.1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var page = window.location.pathname;
 var pageHTML = "/paginas/html5/html5.html";
 var pageC = "/paginas/c/c.html";
-var dataSourceCard = null;
+var datasourceCard = null;
 var datasourceCardHTML = null;
 var datasourceCardC = null;
 
@@ -130,11 +130,11 @@ datasourceCardC = [{
 ]
 
 if (window.location.pathname == pageHTML) {
-    dataSourceCard = datasourceCardHTML;
+    datasourceCard = datasourceCardHTML;
 } else if (window.location.pathname == pageC) {
-    dataSourceCard = datasourceCardC;
+    datasourceCard = datasourceCardC;
 } else {
-    dataSourceCard = null;
+    datasourceCard = null;
 }
 
 var iconStar = "M19.998,6.766l-5.759-0.544c-0.362-0.032-0.676-0.264-0.822-0.61l-2.064-4.999 c-0.329-0.825-1.5-0.825-1.83,0L7.476,5.611c-0.132,0.346-0.462,0.578-0.824,0.61L0.894,6.766C0.035,6.848-0.312,7.921,0.333,8.499 l4.338,3.811c0.279,0.246,0.395,0.609,0.314,0.975l-1.304,5.345c-0.199,0.842,0.708,1.534,1.468,1.089l4.801-2.822 c0.313-0.181,0.695-0.181,1.006,0l4.803,2.822c0.759,0.445,1.666-0.23,1.468-1.089l-1.288-5.345 c-0.081-0.365,0.035-0.729,0.313-0.975l4.34-3.811C21.219,7.921,20.855,6.848,19.998,6.766z";
