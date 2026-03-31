438c23f35df630bcfabe61b94a608a6063a1d9b8
vitoriaKetillin <vitoriaketillin@hotmail.com>
2026-03-31 17:32:10 -0300

    build

--- _next/static/lPHwOjwXE5izC3IuhdQnc/_buildManifest.js
+++ _next/static/lPHwOjwXE5izC3IuhdQnc/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/viKetillin.github.io//_next/:path+",
-        "destination": "/viKetillin.github.io/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
